b4f5e5dd5cc8470f45627e96a0cc14e083884e17
jay herbert <you@example.com>
2018-09-23 20:55:35 +0100

    fix to naviagtion

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -27,6 +27,7 @@ export class DashboardComponent implements OnInit {
   config = {
     sideNav: [
       { name: "Home", routerLink: "../", icon: "home" },
+      { name: "Dashboard", routerLink: "../dashboard", icon: "dashboard" },
       { name: "Account", routerLink: "../account", icon: "account_circle" },
       { name: "Lookup Table", routerLink: "../lookup-maintenance", icon: "folder" },
       { name: "Help", routerLink: "../account", icon: "help_outline" },
--- src/app/profile/account/account.component.ts
+++ src/app/profile/account/account.component.ts
@@ -17,6 +17,7 @@ export class AccountComponent implements OnInit {
   config = {
     sideNav: [
       { name: "Home", routerLink: "../", icon: "home" },
+      { name: "Dashboard", routerLink: "../dashboard", icon: "dashboard" },
       { name: "Account", routerLink: "../account", icon: "account_circle" },
       { name: "Lookup Table", routerLink: "../lookup-maintenance", icon: "folder" },
       { name: "Help", routerLink: "../account", icon: "help_outline" },
